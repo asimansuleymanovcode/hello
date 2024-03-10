0a3ac4d7086f9462d1fb1a337d7e8898ba71be19
Asiman Suleymanov <asiman.suleymanov@kibrit.tech>
2024-03-10 21:00:46 +0400

    initial update

---
+++ src/main/java/az/code/demowebapi/DemoWebApiApplication.java
@@ -0,0 +1,13 @@
+package az.code.demowebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/az/code/demowebapi/controller/MainController.java
@@ -0,0 +1,12 @@
+package az.code.demowebapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping
+    public String sayHello(){
+        return "Hello";
+    }
+}
